941e148e27c429779fabefc144e5315f9e2dd85a
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-17 17:49:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(speakers)
   count = 1 
   speakers.each do |speaker|
     rooms.push("Hello, #{speaker}! You'll be assigned to room #{counter}!")
-    counter +=
+    counter += 1
   end
   return rooms
 end
